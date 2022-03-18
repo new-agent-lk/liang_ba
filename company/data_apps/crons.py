6b6cf18a54759d87076ba2a5247a3f01916cf5cd
levi <leviliang@vectorbuilder.net>
2022-03-18 16:35:04 +0800

    # feat:stock market data

--- company/data_apps/crons.py
+++ company/data_apps/crons.py
@@ -1,14 +1,26 @@
 # 静态化首页
 import os
 import requests
-from datetime import datetime
 import chinese_calendar
 
-# from django.conf import settings
-#
+from datetime import datetime
+
 from data_apps.models import GenericStockMarketData
 
 rq = requests.session()
+
+
+def get_on_work_time(now):
+    # 范围时间
+    a_time = datetime.strptime(str(datetime.now().date()) + '9:00', '%Y-%m-%d%H:%M')
+    b_time = datetime.strptime(str(datetime.now().date()) + '11:30', '%Y-%m-%d%H:%M')
+    c_time = datetime.strptime(str(datetime.now().date()) + '13:00', '%Y-%m-%d%H:%M')
+    d_time = datetime.strptime(str(datetime.now().date()) + '15:00', '%Y-%m-%d%H:%M')
+    # 判断当前时间是否在范围时间内
+    if b_time > now > a_time or d_time > now > c_time:
+        return True
+    else:
+        return False
 
 
 def get_daily_stock_data():
@@ -18,26 +30,41 @@ def get_daily_stock_data():
     print('%s: get_daily_stock_data' % datetime.now())
     now = datetime.now()
     if chinese_calendar.is_workday(now):
-        url = 'http://hq.sinajs.cn/?format=text&list=sz399001'
-        try:
-            r = rq.get(url, headers={"Referer": "https://finance.sina.com.cn"}, timeout=300, verify=False)
-            meta_string = r.text
-            stock_code, stock_string, *_ = r.text.split('=')
-            # print(stock_string.split(','))
-            stock_name, open_price, close_price, now_price, high_price, low_price, *_ = stock_string.split(',')
-            turnover_of_shares = stock_string.split(',')[8]
-            trading_volume = stock_string.split(',')[9]
-            d = stock_string.split(',')[-3]
-            t = stock_string.split(',')[-2]
-            current_time = datetime.strptime(f'{d} {t}', "%Y-%m-%d %H:%M:%S")
-
-            GenericStockMarketData.objects.get_or_create()
-            print(current_time)
-            print(turnover_of_shares, trading_volume, d, t)
-
-        except requests.RequestException:
+        if get_on_work_time(now):
+            print(f'现在是工作时间 {now}')
+            url = 'http://hq.sinajs.cn/?format=text&list=sz399001'
+            r = None
+            try:
+                r = rq.get(url, headers={"Referer": "https://finance.sina.com.cn"}, timeout=300, verify=False)
+            except requests.RequestException:
+                pass
+            if r:
+                meta_string = r.text
+                stock_code, stock_string, *_ = r.text.split('=')
+                stock_name, open_price, close_price, now_price, high_price, low_price, *_ = stock_string.split(',')
+                turnover_of_shares = stock_string.split(',')[8]
+                trading_volume = stock_string.split(',')[9]
+                d = stock_string.split(',')[-3]
+                t = stock_string.split(',')[-2]
+                current_time = datetime.strptime(f'{d} {t}', "%Y-%m-%d %H:%M:%S")
+                data = {
+                    "stock_code": stock_code,
+                    "stock_name": stock_name,
+                    "now_price": now_price,
+                    "open_price": open_price,
+                    "close_price": close_price,
+                    "high_price": high_price,
+                    "low_price": low_price,
+                    "turnover_of_shares": turnover_of_shares,
+                    "trading_volume": trading_volume,
+                    "current_time": current_time,
+                    "source_data": meta_string,
+                }
+                print(f'获取到数据  {meta_string}')
+                try:
+                    gsmd, _ = GenericStockMarketData.objects.get_or_create(**data)
+                    print(f'{now}  {gsmd.stock_code} now_price:{gsmd.now_price}')
+                except Exception as e:
+                    print(e)
+        else:
             pass
-
-
-if __name__ == '__main__':
-    get_daily_stock_data()
