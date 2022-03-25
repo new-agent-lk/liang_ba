248810397e458812dcbf4898160b65394c80c51d
levi <leviliang@vectorbuilder.net>
2022-03-25 15:49:50 +0800

    #

--- company/data_apps/view_set/generic_stock_market_data.py
+++ company/data_apps/view_set/generic_stock_market_data.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import django_filters
 from django_filters.rest_framework import FilterSet
 from rest_framework.viewsets import ModelViewSet
 
@@ -7,6 +8,8 @@ from data_apps.serializers.generic_stock_market_data import GenericStockMarketDa
 
 
 class GenericStockMarketDataFilter(FilterSet):
+    create_time = django_filters.DateTimeFromToRangeFilter(field_name='create_time', lookup_expr='gte')
+
     class Meta:
         model = GenericStockMarketData
         fields = [
