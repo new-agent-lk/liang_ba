9dca37bc325268ccd37a728a762c6c28cb0ffada
levi <leviliang@vectorbuilder.net>
2022-03-25 15:54:34 +0800

    #

--- company/data_apps/view_set/generic_stock_market_data.py
+++ company/data_apps/view_set/generic_stock_market_data.py
@@ -9,6 +9,15 @@ from data_apps.serializers.generic_stock_market_data import GenericStockMarketDa
 
 class GenericStockMarketDataFilter(FilterSet):
     create_time = django_filters.DateTimeFromToRangeFilter(field_name='create_time', lookup_expr='gte')
+    current_time = django_filters.DateTimeFromToRangeFilter(field_name='current_time', lookup_expr='gte')
+    stock_name = django_filters.CharFilter(field_name='stock_name')
+    now_price = django_filters.CharFilter(field_name='now_price')
+    open_price = django_filters.CharFilter(field_name='open_price')
+    close_price = django_filters.CharFilter(field_name='close_price')
+    high_price = django_filters.CharFilter(field_name='high_price')
+    low_price = django_filters.CharFilter(field_name='low_price')
+    turnover_of_shares = django_filters.CharFilter(field_name='turnover_of_shares')
+    trading_volume = django_filters.CharFilter(field_name='trading_volume')
 
     class Meta:
         model = GenericStockMarketData
