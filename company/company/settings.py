696c715601aacd8e1fc37740b212513eae2cb7d3
funny <funnychen@vectorbuilder.ne>
2022-06-20 14:46:21 +0800

    123

--- company/company/settings.py
+++ company/company/settings.py
@@ -6,13 +6,14 @@ DEBUG = True
 
 DATABASES = {
     'default': {
+        'TEST_CHARSET': 'utf8',
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'liang_ba',   # 请换成新建的数据库名称
         'USER': 'root',
-        'PASSWORD': 'LCJKKwti',  # 请换成自己的密码
-        'HOST': '127.0.0.1',  # 如果不能连接，改成localhost
+        'PASSWORD': 'mysql',  # 请换成自己的密码
+        'HOST': 'localhost',  # 如果不能连接，改成localhost
         'POST': '3306',
     }
 }
--- company/manage.py
+++ company/manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line util`ity for administrative tasks."""
 import os
 import sys
 
