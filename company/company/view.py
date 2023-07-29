cfba09829f4296b2e81946072a2990b91bf56ab5
Levi Liang <leviliang@vectorbuilder.net>
2023-07-29 19:24:58 +0800

    # update

--- company/company/view.py
+++ company/company/view.py
@@ -4,5 +4,5 @@ from django.views.generic import RedirectView
 
 class IndexView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('wagtail_serve:liang-ba')
+        return reverse('wagtail_serve', args=('liang-ba/',))
 
