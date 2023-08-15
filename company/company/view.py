a8302a20fc69aacd0de05d6d193c5e3bfd31048a
Levi Liang <leviliang@vectorbuilder.net>
2023-08-15 11:16:31 +0800

    # update

--- company/company/view.py
+++
@@ -1,8 +0,0 @@
-from django.urls import reverse
-from django.views.generic import RedirectView
-
-
-class IndexView(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        return reverse('wagtail_serve', args=('liang-ba/',))
-
--- company/static/js/mobile.js
+++
@@ -1,2 +0,0 @@
-﻿
-if((navigator.userAgent.toLowerCase().match(/(iPhone|iPod|Android|ios)/i))){if(typeof(murl)=="undefined"){var murl="";}location.href=webroot+murl;}
